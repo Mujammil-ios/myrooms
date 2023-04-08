ed7f74681c4b46d1a25cc83986833e5050685b9b
aeiou-st <54111043+Mujammil-ios@users.noreply.github.com>
2023-04-08 17:49:44 +0500

    solved conflict

--- app/src/main/java/com/mj/myrooms/ui/owner/OwnerLandingActivity.java
+++ app/src/main/java/com/mj/myrooms/ui/owner/OwnerLandingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mj.myrooms.R;
+
 public class OwnerLandingActivity extends AppCompatActivity {
 
     @Override
