6f4c128c225cf1c5e768fc2027ad37aef5b10d7e
aeiou-st <54111043+Mujammil-ios@users.noreply.github.com>
2023-04-08 17:04:50 +0500

    update

---
+++ app/src/main/java/com/mj/myrooms/ui/activity/customer/CustomerLandingActivity.java
@@ -0,0 +1,14 @@
+package com.mj.myrooms.ui.activity.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_landing);
+    }
+}
---
+++ app/src/main/java/com/mj/myrooms/ui/activity/owner/OwnerLandingActivity.java
@@ -0,0 +1,14 @@
+package com.mj.myrooms.ui.activity.owner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_landing);
+    }
+}
