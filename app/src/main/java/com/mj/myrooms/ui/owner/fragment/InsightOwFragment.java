f349031a4b3b7bac29780f12b7d129e5ca6a2ac1
rehanpinjari <rehanpinjari04321@gmail.com>
2023-08-19 23:40:14 +0500

    added UI

--- app/src/main/java/com/mj/myrooms/ui/owner/fragment/InsightOwFragment.java
+++ app/src/main/java/com/mj/myrooms/ui/owner/fragment/InsightOwFragment.java
@@ -25,7 +25,6 @@ public class InsightOwFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//         Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_insight_ow, container, false);
     }
 }
