1fd55b8163d20e6a0ed188ac28b8094ecd230770
Mujammil-ios <mujammilpinjari68507@gmail.com>
2023-08-20 00:24:38 +0500

    up

--- app/src/main/java/com/mj/myrooms/ui/owner/fragment/InsightOwFragment.java
+++ app/src/main/java/com/mj/myrooms/ui/owner/fragment/InsightOwFragment.java
@@ -25,7 +25,7 @@ public class InsightOwFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//         Inflate the layout for this fragment
+//         Inflate the layout for this fragment hh
         return inflater.inflate(R.layout.fragment_insight_ow, container, false);
     }
 }
